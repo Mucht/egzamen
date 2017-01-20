7fb1da077783cdf59db0e6653dcd16a5ad6c9ef0
Mathieu Claessens <mathieu_claessens@hotmail.com>
2017-01-20 17:43:08 +0100

    Correcting typo mistakes

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@
  * started at 19/01/2017
 */
 
-import { init an initDB } from "./core/mongodb";
+import { init as initDB } from "./core/mongodb";
 import { init as initExpress } from "./core/express";
 
 const APP_PORT = 12345;
