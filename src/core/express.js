d09b5da6e26efbc988ae129e29702b7be8a4215e
Mathieu Claessens <mathieu_claessens@hotmail.com>
2017-01-21 06:37:19 +0100

    Add router for exports

--- src/core/express.js
+++ src/core/express.js
@@ -12,6 +12,7 @@ import responseTime from "response-time";
 import mitanEko from "mitan-eko";
 import zouti from "zouti";
 import systemRoutes from "../routes/system";
+import exportsRoutes from "../routes/exports";
 
 const APP_PORT = "12345";
 
@@ -19,7 +20,7 @@ let oApp,
     fInit;
 
 fInit = function( iAppPort = APP_PORT ) {
-    if( oApp ) {
+    if ( oApp ) {
         return oApp;
     }
 
@@ -30,15 +31,16 @@ fInit = function( iAppPort = APP_PORT ) {
     oApp.use( responseTime() );
     oApp.use( bodyParser.json() );
     oApp.use( bodyParser.urlencoded( {
-        "extended": true;
+        "extended": true,
     } ) );
 
     // routes
     oApp.use( systemRoutes );
+    oApp.use( exportsRoutes );
 
     // listening
     oApp.listen( iAppPort, () => {
-        zouti.success( `Server is lestening on ${ iAppPort }`, "egzamen" )
+        zouti.success( `Server is lestening on ${ iAppPort }`, "egzamen" );
     } );
 };
 
---
+++ src/routes/exports.js
@@ -0,0 +1,25 @@
+/* RIA/egzamen
+ *
+ * /src/routes/exports/js - API Routes for exports
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 20/01/2017
+*/
+
+import { Router } from "express";
+
+// import list from "../controllers/exports/list";
+// import details from "../controllers/exports/details";
+// import create from "../controllers/exports/create";
+// import update from "../controllers/exports/update";
+// import destroy from "../controllers/exports/destroy";
+
+let oRouter = new Router();
+
+// oRouter.get( "/exports", list );
+// oRouter.get( "/exports/:slug", details );
+// oRouter.post( "/exports", create );
+// oRouter.patch( "/exports", update );
+// oRouter.delete( "/exports", destroy );
+
+export default oRouter;
