517bdc5d355e55cfddc2c776cc7e458d1fca555f
Mathieu Claessens <mathieu_claessens@hotmail.com>
2017-01-23 20:13:48 +0100

    Install dependencies & add base modules

---
+++ static/modules/main.js
@@ -0,0 +1,13 @@
+/* RIA/egzamen
+ *
+ * /static/modules/main.js - Main entry file
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 23/01/2017
+*/
+
+import utils from "./utils";
+
+console.log( "Booom !" );
+
+utils();
---
+++ static/modules/utils.js
@@ -0,0 +1,11 @@
+/* RIA/egzamen
+ *
+ * /static/modues/utils.js - Small utils
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 23/01/2017
+*/
+
+export default function() {
+    console.log( "Booom utils !" );
+}
